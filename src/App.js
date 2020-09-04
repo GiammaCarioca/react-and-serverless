bb1a5c2aa209e1083b995765fddc4f224299cc65
Thiago Giammattey <giammattey@gmail.com>
2020-09-04 11:13:13 -0300

    add routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Game from './pages/Game'
+import HighScores from './pages/HighScores'
+import GameOver from './pages/GameOver'
 
 function App() {
 	return (
-		<div>
-			<h1>App</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/game" component={Game} />
+				<Route path="/highScores" component={HighScores} />
+				<Route path="/gameOver" component={GameOver} />
+			</Switch>
+		</Router>
 	)
 }
 
