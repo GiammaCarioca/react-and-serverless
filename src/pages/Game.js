b13e374fd7049e7ce29f944dbf5ab0109a750eb3
Thiago Giammattey <giammattey@gmail.com>
2020-09-04 12:12:13 -0300

    styled the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
+import {
+	StyledGame,
+	StyledCharacter,
+	StyledScore,
+	StyledTimer,
+} from '../styled/Game'
+
+import { Strong } from '../styled/Random'
+
 export default function Game() {
-	return <h1>Game</h1>
+	return (
+		<StyledGame>
+			<StyledScore>
+				Score: <Strong>0</Strong>
+			</StyledScore>
+			<StyledCharacter>A</StyledCharacter>
+			<StyledTimer>
+				Time: <Strong>00:000</Strong>
+			</StyledTimer>
+		</StyledGame>
+	)
 }
