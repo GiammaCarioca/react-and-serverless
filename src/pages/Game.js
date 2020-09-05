9e568cd520e566e17abadc3a7239b334e2d86322
Thiago Giammattey <giammattey@gmail.com>
2020-09-05 10:43:29 -0300

    added logic to update user score based on input

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import {
 	StyledGame,
@@ -11,26 +11,19 @@ import { Strong } from '../styled/Random'
 
 // Since Game is a page component it will receive a property of history from React Router that we can use to navigate
 export default function Game({ history }) {
-	const [score] = useState(0)
 	const MAX_SECONDS = 5
+	const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+	const [score, setScore] = useState(0)
 	const [ms, setMs] = useState(999) // milliseconds
 	const [seconds, setSeconds] = useState(MAX_SECONDS)
+	const [currentCharacter, setCurrentCharacter] = useState('')
 
 	useEffect(() => {
 		const currentTime = new Date()
 		const interval = setInterval(() => updateTime(currentTime), 1)
 
-		const addLeadingZeros = (str, length) => {
-			let zeros = ''
-
-			for (let i = 0; i < length; i++) {
-				zeros += '0'
-			}
-
-			return (zeros + str).slice(-length)
-		}
-
-		// usually you’ll want to declare functions needed by an effect inside of it.
+		// usually you’ll want to declare functions needed by an effect inside of it
 		const updateTime = (startTime) => {
 			const endTime = new Date()
 			// console.log(endTime.getTime())
@@ -38,9 +31,9 @@ export default function Game({ history }) {
 
 			const msPassedStr = (endTime.getTime() - startTime.getTime()).toString()
 
-			//add zeros if necessary to ensure the string has exactly 5 characters
+			// add zeros if necessary to ensure the string has exactly 5 characters
 			const formattedMsString = ('0000' + msPassedStr).slice(-5)
-			//0000 - first 2 are the seconds, and the last 3 are the ms
+			// 0000 - first 2 are the seconds, and the last 3 are the ms
 
 			const updatedSeconds =
 				MAX_SECONDS - parseInt(formattedMsString.substring(0, 2)) - 1
@@ -53,6 +46,19 @@ export default function Game({ history }) {
 			setMs(addLeadingZeros(updatedMs, 3))
 		}
 
+		const addLeadingZeros = (str, length) => {
+			let zeros = ''
+
+			for (let i = 0; i < length; i++) {
+				zeros += '0'
+			}
+
+			return (zeros + str).slice(-length)
+		}
+
+		setRandomCharacter()
+
+		// cleanin up
 		return () => {
 			clearInterval(interval)
 		}
@@ -65,24 +71,42 @@ export default function Game({ history }) {
 		}
 	}, [seconds, ms, history]) // history is a dependency because our effect actually uses it)
 
-	useEffect(() => {
-		const keyUpHandler = (e) => {
-			console.log(e.key)
-		}
+	const keyUpHandler = useCallback(
+		(e) => {
+			console.log(e.key, currentCharacter)
 
+			if (e.key === currentCharacter) {
+				setScore((prevScore) => prevScore + 1)
+			} else {
+				if (score > 0) {
+					setScore((prevScore) => prevScore - 1)
+				}
+			}
+			setRandomCharacter()
+		},
+		[currentCharacter, score]
+	)
+
+	useEffect(() => {
 		document.addEventListener('keyup', keyUpHandler)
 
 		return () => {
 			document.removeEventListener('keyup', keyUpHandler)
 		}
-	}, [])
+	}, [keyUpHandler])
+
+	// Helper Functions
+	const setRandomCharacter = () => {
+		const randomInt = Math.floor(Math.random() * 36)
+		setCurrentCharacter(characters[randomInt])
+	}
 
 	return (
 		<StyledGame>
 			<StyledScore>
 				Score: <Strong>{score}</Strong>
 			</StyledScore>
-			<StyledCharacter>A</StyledCharacter>
+			<StyledCharacter>{currentCharacter}</StyledCharacter>
 			<StyledTimer>
 				Time:{' '}
 				<Strong>
