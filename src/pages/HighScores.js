d462f819cb974c64532df00cb5cc85818016f5f7
Thiago Giammattey <giammattey@gmail.com>
2020-09-06 09:16:05 -0300

    displaying high scores

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ScoreLI } from '../styled/HighScores'
 
 export default function HighScores() {
-	return <h1>High Scores</h1>
+	//display those scores
+	const [highScores, setHighScores] = useState([])
+
+	//use the fetch API to call getHighScores function
+	useEffect(() => {
+		const loadHighScores = async () => {
+			try {
+				// relative path to our serverless function
+				const res = await fetch('/.netlify/functions/getHighScores')
+				const scores = await res.json()
+				setHighScores(scores)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		loadHighScores()
+	}, [])
+
+	return (
+		<>
+			<h1>High Scores</h1>
+			<ol>
+				{highScores.map((score) => (
+					<ScoreLI key={score.id}>
+						{score.fields.name} - {score.fields.score}
+					</ScoreLI>
+				))}
+			</ol>
+		</>
+	)
 }
---
+++ src/styled/HighScores.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const ScoreLI = styled.li`
+	font-size: 1.6rem;
+	margin-bottom: 0.5rem;
+`
